9d9fdf593cf38d424fa281f9651be5afaa00dd3e
Changjian Zhang <mljzcj@gmail.com>
2022-12-29 10:55:09 -0500

    clean up code

--- scripts/run_car_circle.py
+++ scripts/run_car_circle.py
@@ -26,22 +26,10 @@ sys_eval = CMASystemEvaluator(
 solver = CMASolver(0.1, sys_eval)
 evaluator = Evaluator(prob, solver)
 
-# solver.set_options({'evals': 25})
-# logger = Logger()
-# for i in range(10):
-#     delta, dist = evaluator.min_violation(logger=logger)
-#     if dist < 0.1:
-#         print(logger.find_dev_result(i, delta))
-#         break
-
 # from datetime import datetime
 # start = datetime.now()
 # print(sys_eval.eval_sys([20, 0.5], prob))
 # print(datetime.now() - start)
-
-# delta = scale([0.12102787, 0.44451285], env.get_dev_bounds())
-# print('\nDistance:', prob.dist.eval_dist(delta), '\n')
-# evaluator.visualize_violation(delta, x0=[-0.10101447,  0.32559958], render=True)
 
 experiment = Experiment(evaluator)
 data1, _ = experiment.run_diff_max_samples('CMA', np.arange(25, 126, 25), out_dir='data/car-circle-ppo/cma')
@@ -69,24 +57,6 @@ plt.ylabel('Minimum distance')
 boxplot([data1, data2], ['red', 'blue'], np.arange(25, 126, 25) * (1 + solver.options()['restarts']),
         ['CMA', 'Random'])
 plt.savefig('gifs/car-circle-ppo/sample-boxplot.png', bbox_inches='tight')
-
-# import time
-# import numpy as np
-
-# inst, _ = env.instantiate(env.get_delta_0(), render=True)
-# rewards = []
-# for _ in range(1):
-#     total = 0
-#     obs = inst.reset_to([-0.10101447,  0.32559958])
-#     # time.sleep(2)
-#     for _ in range(300):
-#         obs, reward, _, _ = inst.step(agent.next_action(obs))
-#         total += reward
-#         time.sleep(0.01)
-#     # print(total)
-#     rewards.append(total)
-# inst.close()
-# print('Mean rewards:', np.mean(rewards))
 
 sys_eval3 = ExpectationSysEvaluator(
     phi,
