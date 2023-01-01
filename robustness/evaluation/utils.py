60344b4daa1f5670a46aadc96684d8de392f421a
Changjian Zhang <mljzcj@gmail.com>
2023-01-01 18:21:16 -0500

    updat figures

--- robustness/evaluation/utils.py
+++ robustness/evaluation/utils.py
@@ -9,12 +9,12 @@ def _set_box_color(bp, color):
     plt.setp(bp['medians'], color=color)
 
 
-def boxplot(datas, colors, labels, legends):
+def boxplot(datas, colors, labels, legends, **kwargs):
     num_group = len(datas)
     num_each_group = len(datas[0])
     total = num_group * num_each_group
     for i in range(num_group):
-        bp = plt.boxplot(datas[i], positions=np.arange(i+1, total+1, num_group)+np.arange(num_each_group), showfliers=False)
+        bp = plt.boxplot(datas[i], positions=np.arange(i+1, total+1, num_group)+np.arange(num_each_group), **kwargs)
         _set_box_color(bp, colors[i])
     
     plt.xticks(np.arange(1+(num_group-1)/2, total, num_group)+np.arange(num_each_group), labels=labels)
