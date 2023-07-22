af1fd5e5cba15fa45cb1575a933082df0c084537
Changjian Zhang <mljzcj@gmail.com>
2023-07-22 18:47:52 -0400

    Add AFC scripts

---
+++ scripts/testing/AFC_RL.py
@@ -0,0 +1,80 @@
+import os
+import matplotlib.pyplot as plt
+
+from matlab import engine
+from robustness.agents.matlab import RLAgent
+from robustness.analysis.algorithms.breach import BreachSystemEvaluator
+from robustness.analysis.algorithms.cma import CMASolver
+from robustness.analysis.algorithms.random import RandomSolver
+from robustness.analysis.breach import BreachSTL
+from robustness.analysis.core import Problem
+from robustness.analysis.utils import L2Norm
+from robustness.envs.matlab import DevAFC
+from robustness.evaluation.evaluator import Evaluator
+from robustness.evaluation.experiment import Experiment
+
+os.makedirs('gifs/AFC/RL', exist_ok=True)
+plt.rc('axes', labelsize=17, titlesize=17)
+plt.rc('xtick', labelsize=14)
+plt.rc('ytick', labelsize=14)
+plt.rc('legend', fontsize=14)
+
+eng = engine.start_matlab()
+eng.addpath('lib/breach', nargout=0)
+eng.addpath(eng.genpath('models/AFC'), nargout=0)
+eng.InitBreach(nargout=0)
+
+MAF_sensor_tols = [0.95, 1.05]
+AF_sensor_tols = [0.99, 1.01]
+env = DevAFC(eng, MAF_sensor_tols, AF_sensor_tols, (1.0, 1.0))
+agent = RLAgent('AFC_DDPG_Agent.mat')
+phi = BreachSTL('alw (AF[t] < 1.2*14.7 and AF[t] > 0.8*14.7)')
+
+prob = Problem(env, agent, phi, L2Norm(env))
+sys_eval = BreachSystemEvaluator(eng, phi, {'restarts': 1, 'evals': 50})
+
+# Use CMA
+solver = CMASolver(0.2, sys_eval, {'restarts': 1, 'evals': 50})
+evaluator = Evaluator(prob, solver)
+experiment = Experiment(evaluator)
+
+print('Find violations by CMA...')
+records_cma = experiment.record_min_violations(out_dir='data/AFC/RL/cma')
+records_cma, violations_cma = experiment.summarize_violations(records_cma)
+# Plot all samples
+for i in range(len(records_cma)):
+    samples = [(X, Y) for (X, Y, _) in records_cma[i]]
+    experiment.plot_samples(samples, 'MAF Sensor Tolerance', 'AF Sensor Tolerance', 'data/AFC/RL', n=20)
+    plt.title('Violations found by CMA')
+    plt.savefig(f'gifs/AFC/RL/fig-violations-cma-{i}.png', bbox_inches='tight')
+
+# Find the minimum violation and certify an unsafe region
+min_violation = experiment.min_violation_of_all(violations_cma)
+if min_violation is not None:
+    radius = evaluator.unsafe_region(min_violation, 0.1, 0.05, 'data/AFC/RL', n=1000)
+    experiment.plot_unsafe_region(min_violation, radius, 'MAF Sensor Tolerance', 'AF Sensor Tolerance', 'data/AFC/RL', n=20)
+    plt.title('Unsafe region found by CMA')
+    plt.savefig('gifs/AFC/RL/fig-unsafe-region-cma.png', bbox_inches='tight')
+
+# Use random search
+solver = RandomSolver(sys_eval, {'restarts': 1, 'evals': 50})
+evaluator = Evaluator(prob, solver)
+experiment = Experiment(evaluator)
+
+print('Find violations by random search...')
+records_random = experiment.record_min_violations(out_dir='data/AFC/RL/random')
+records_random, violations_random = experiment.summarize_violations(records_random)
+# Plot all samples
+for i in range(len(records_random)):
+    samples = [(X, Y) for (X, Y, _) in records_random[i]]
+    experiment.plot_samples(samples, 'MAF Sensor Tolerance', 'AF Sensor Tolerance', 'data/AFC/RL', n=20)
+    plt.title('Violations found by random search')
+    plt.savefig(f'gifs/AFC/RL/fig-violations-random-{i}.png', bbox_inches='tight')
+
+# Find the minimum violation and certify an unsafe region
+min_violation = experiment.min_violation_of_all(violations_random)
+if min_violation is not None:
+    radius = evaluator.unsafe_region(min_violation, 0.1, 0.05, 'data/AFC/RL', n=1000)
+    experiment.plot_unsafe_region(min_violation, radius, 'MAF Sensor Tolerance', 'AF Sensor Tolerance', 'data/AFC/RL', n=20)
+    plt.title('Unsafe region found by random search')
+    plt.savefig('gifs/AFC/RL/fig-unsafe-region-random.png', bbox_inches='tight')
