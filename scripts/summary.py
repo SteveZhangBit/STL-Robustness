a93c8816524a0862c5d0e3ffc7c9e9dec119390e
Changjian Zhang <mljzcj@gmail.com>
2023-09-15 15:25:51 -0400

    Add Acc3 data

--- scripts/summary.py
+++ scripts/summary.py
@@ -2,10 +2,24 @@ import numpy as np
 import pandas as pd
 
 
+def read_data(data_dir):
+    data = pd.read_csv(data_dir)
+    data['Violations/Total'] = data.apply(lambda row: f"{row['Num of violations']:.0f}/{row['Num of samples']:.0f}", axis=1)
+    data['Violation Distance'] = data.apply(
+        lambda row: f"{row['Mean distance']:.2f}$\\pm${row['Std distance']:.2f} ({row['Min distance']:.2f}-{row['Max distance']:.2f})",
+        axis=1)
+    return data
+
+def summarize(data_dirs, names, kind, out):
+    datas = [read_data(f'data/{data_dir}/{kind}/summary.csv') for data_dir in data_dirs]
+    data = pd.concat(datas, keys=names)
+    data.index.names = ['Problem', 'Trial']
+    print(data)
+    data[['Violations/Total', 'Violation Distance', 'Total time (s)']].to_csv(out, float_format='%.2f')
+
 data_dirs = [
     'cartpole-pid',
     'cartpole-dqn',
-    'cartpole4-pid',
     'lunar-lander-lqr',
     'lunar-lander-ppo',
     'car-circle-ppo',
@@ -21,7 +35,6 @@ data_dirs = [
 names = [
     'cartpole-pid',
     'cartpole-dqn',
-    'cartpole4-pid',
     'lunar-lander-lqr',
     'lunar-lander-ppo',
     'car-circle-ppo',
@@ -34,22 +47,22 @@ names = [
     'WTK-traditional',
 ]
 
-def read_data(data_dir):
-    data = pd.read_csv(data_dir)
-    data['Violations/Total'] = data.apply(lambda row: f"{row['Num of violations']:.0f}/{row['Num of samples']:.0f}", axis=1)
-    data['Violation Distance'] = data.apply(
-        lambda row: f"{row['Mean distance']:.2f}$\\pm${row['Std distance']:.2f} ({row['Min distance']:.2f}-{row['Max distance']:.2f})",
-        axis=1)
-    return data
+summarize(data_dirs, names, 'cma', 'data/cma-summary.csv')
+summarize(data_dirs, names, 'random', 'data/random-summary.csv')
 
-cma_datas = [read_data(f'data/{data_dir}/cma/summary.csv') for data_dir in data_dirs]
-cma_data = pd.concat(cma_datas, keys=names)
-cma_data.index.names = ['Problem', 'Trial']
-print(cma_data)
-cma_data[['Violations/Total', 'Violation Distance', 'Total time (s)']].to_csv('data/cma-summary.csv', float_format='%.2f')
+data_dirs = [
+    'cartpole4-pid',
+    'lunarlander3-ppo',
+    'car-circle3-ppo',
+    'ACC3/traditional'
+]
 
-random_datas = [read_data(f'data/{data_dir}/random/summary.csv') for data_dir in data_dirs]
-random_data = pd.concat(random_datas, keys=names)
-random_data.index.names = ['Problem', 'Trial']
-print(random_data)
-random_data[['Violations/Total', 'Violation Distance', 'Total time (s)']].to_csv('data/random-summary.csv', float_format='%.2f')
+names = [
+    'cartpole4-pid',
+    'lunarlander3-ppo',
+    'car-circle3-ppo',
+    'ACC3-traditional'
+]
+
+summarize(data_dirs, names, 'cma', 'data/cma-summary-2.csv')
+summarize(data_dirs, names, 'random', 'data/random-summary-2.csv')
