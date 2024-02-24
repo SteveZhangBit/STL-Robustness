4966c06746803ff00d380482550dfa205bbe6ea7
Changjian Zhang <mljzcj@gmail.com>
2024-02-24 11:23:57 -0500

    Bug fix in one-layer matlab and rerun WTK T and RL

--- robustness/analysis/algorithms/MyFalsificationProblem.m
+++ robustness/analysis/algorithms/MyFalsificationProblem.m
@@ -10,8 +10,8 @@ methods
     function this = MyFalsificationProblem(dev_names, dev_0, dev_bounds, BrSys, phi)
         this = this@FalsificationProblem(BrSys, phi);
         this.dev_names = dev_names;
-        this.dev_0 = rescale(dev_0, dev_bounds(:, 1), dev_bounds(:, 2));
         this.dev_bounds = dev_bounds;
+        this.dev_0 = this.normalize_delta(dev_0);
 
         this.dev_param_idx = zeros(size(dev_names));
         for i = 1:length(dev_names)
@@ -23,9 +23,13 @@ methods
         [obj, cval, x_stoch] = this.objective_fn@FalsificationProblem(x);
         
         delta = x(this.dev_param_idx, :);
-        delta = rescale(delta, this.dev_bounds(:, 1), this.dev_bounds(:, 2));
+        delta = this.normalize_delta(delta);
         dist = sqrt(sum((delta - this.dev_0).^2));
         obj = obj + dist;
+    end
+
+    function [result] = normalize_delta(this, delta)
+        result = (delta - this.dev_bounds(:, 1)) ./ (this.dev_bounds(:, 2) - this.dev_bounds(:, 1));
     end
 
 end
