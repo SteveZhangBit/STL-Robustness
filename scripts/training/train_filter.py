56e43427914a134064a9a4c879a7a3a391553559
Parv Kapoor <parvk@gs21615.sp.cs.cmu.edu>
2024-06-11 14:05:03 -0400

    added

--- scripts/training/train_filter.py
+++ scripts/training/train_filter.py
@@ -1,32 +1,47 @@
 import os
 from hyperpol import HyperPolicy
 import torch
-from robustness.envs.lunar_lander import DevLunarLander, SafetyProp, SafetyProp2
+# from robustness.envs.lunar_lander import DevLunarLander, SafetyProp, SafetyProp2
+from robustness.envs.car_run import DevCarRun, SafetyProp, SafetyProp2
 import time
 import numpy as np
 import imageio
 from stable_baselines3 import PPO as BaselinePPO
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
+import pandas as pd
+from datetime import datetime
+import random 
+from robustness.agents.rsrl import PPOVanilla
+
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
 
 class transfer_env():
     
-    def __init__(self, dev, env) -> None:
-        self.episode_len = 300
+    def __init__(self, dev, env, episode_len, agent) -> None:
+        self.episode_len = episode_len
         self.dev = dev
         self.observation_space = env.observation_space()
-        #env, x0bounds = env.instantiate([7.832101630613496,0.4323776925279945])
         self.env, x0bounds = env.instantiate(dev)
         self.parent_env = env
         self.action_space = self.env.action_space
-        #env, x0bounds = env.instantiate([5.879106789841222, 0.5553259436583973])
-        self.base_policy = BaselinePPO.load('/usr0/home/parvk/cj_project/STL-Robustness/models/lunar-lander/ppo.zip', self.env)  
+        # self.base_policy = BaselinePPO.load('/usr0/home/parvk/cj_project/STL-Robustness/models/lunar-lander/ppo2.zip', self.env) 
+        self.base_policy = agent 
         self.phi = SafetyProp()
-        # # set the initial state after
-        # obs = env.reset()
 
-    def set_to_new(self, dev):
-        self.env, x0bounds = self.parent_env.instantiate(dev)
+
+    def set_to_new(self, dev, render=False):
+        if render is True:
+            self.env, x0bounds = self.parent_env.instantiate(dev, render=True)
+        else:
+            self.env, x0bounds = self.parent_env.instantiate(dev)
+        #self.env, x0bounds = self.parent_env.instantiate([7.843638754739874,0.505699763079408])
+        self.env.seed(seed)
+        self.env.action_space.seed(seed)
+        self.env.observation_space.seed(seed)
         return self.env
 
 
@@ -48,7 +63,7 @@ class transfer_env():
 
     # imageio.mimsave('lunar_lander_sat2.gif', frames, fps=30)
 
-def train(filter, fin, dev_file_path, opt):
+def train(filter, fin, dev_file_path, opt, env_name):
     '''
     here take the base policy, the hypernetwork, the deviation file, the env
     then 
@@ -64,14 +79,15 @@ def train(filter, fin, dev_file_path, opt):
     '''
     # load violating devs from a csv
     # currently just repeating values
-    devs = np.tile(np.array([7.832101630613496,0.4323776925279945]), (10, 1))
+    de = pd.read_csv(dev_file_path).to_numpy()
+    devs = de[:,:2]
     num_episodes = 10
-    num_epochs = 10
-    data = []
-    episode_data = []
-    obs_record= []
-    rew_record =[ ]
-    writer = SummaryWriter('runs/experiment_2')
+    num_epochs = 100
+    # data = []
+    # episode_data = []
+    # obs_record= []
+    # rew_record =[ ]
+    writer = SummaryWriter('runs/experiment_car_run_final')
     for epoch in tqdm(range(num_epochs)):
         running_loss = 0.0
         running_score = 0.0
@@ -84,7 +100,10 @@ def train(filter, fin, dev_file_path, opt):
             for episode in range(0,num_episodes):
                 obs = env.reset()
                 for j in range(0, fin.episode_len):
-                    action = fin.base_policy.predict(obs, deterministic=True)[0]
+                    if isinstance(fin.base_policy, BaselinePPO):
+                        action = fin.base_policy.predict(obs, deterministic=True)[0]
+                    else:
+                        action = fin.base_policy.next_action(obs)
                     dev_input = torch.tensor(dev, dtype=torch.float32)
                     nom_ac =  torch.tensor([action], dtype=torch.float32)
                     safe_ac = filter(dev_input, nom_ac)
@@ -111,30 +130,87 @@ def train(filter, fin, dev_file_path, opt):
                 writer.add_scalar('score', running_score / 50, epoch * len(devs) + i)
                 running_loss = 0.0
                 running_score =0.0
+                checkpoint_prefix = 'filter_checkpoint_'
+                checkpoint_filename = checkpoint_prefix + datetime.now().strftime('%Y%m%d_%H%M%S') + '.pth'
+                checkpoint_path = os.path.join(f'model_weights/{env_name}/', checkpoint_filename)
+                torch.save(filter.state_dict(), checkpoint_path)
+                # torch.save(filter.state_dict(), f'model_weights/{}filter_weights.pth')
 
     print('finished training')
     writer.close()
-    torch.save(filter.state_dict(), 'filter_weights.pth')
+    # torch.save(filter.state_dict(), 'model_weights/filter_weights.pth')
 
 
+def eval(filter, fin, use_filter, dev_file_path):
+    '''
+    checks the policy performance on the deviations
+    '''
+    de = pd.read_csv(dev_file_path).to_numpy()
+    devs = de[:,:2]
+    num_episodes = 2
+    data_store = []
+    for i in tqdm(range(0, len(devs))):
+            dev = devs[i]
+            # print(dev)
+            # set to this dev
+            env = fin.set_to_new(dev, render=False)
+            final_loss =0.0
+            for episode in range(0,num_episodes):
+                rewards = 0
+                obs_record = []
+                obs = env.reset()
+                for j in range(0, fin.episode_len):
+                    if isinstance(fin.base_policy, BaselinePPO):
+                        action = fin.base_policy.predict(obs, deterministic=True)[0]
+                    else:
+                        action = fin.base_policy.next_action(obs)
+                    if use_filter is True:
+                        dev_input = torch.tensor(dev, dtype=torch.float32)
+                        nom_ac =  torch.tensor([action], dtype=torch.float32)
+                        safe_ac = filter(dev_input, nom_ac)
+                        
+                        with torch.no_grad():
+                            f = safe_ac.numpy()
+                        # take the safe action, get the obs
+                        obs, reward, _, _ = env.step(f[0])
+                    else:
+                        obs, reward, _, _ = env.step(action)
+
+                    obs_record.append(obs)
+                    rewards+=reward
+                    #env.render()
+
+                       
+                    # squared_diff = (safe_ac - nom_ac) ** 2
+                    # # Compute the mean of the squared differences
+                    # action_penalty = torch.mean(squared_diff)
+                    # final_loss += action_penalty
+                score = fin.phi.eval_trace(np.array([obs]), np.array([reward]))
+                print(f'score is {score}')
+                print(f'reward is {rewards}')
+                #print(final_loss)
+                data_store.append([score, rewards])
     
+    print(data_store)
 
 if __name__ == '__main__':
-    winds = [0.0, 10.0]  
-    turbulences = [0.0, 1.0]
+    # winds = [0.0, 10.0]  
+    # turbulences = [0.0, 1.0]
     
-    #instantiate transfer env
-    fin = transfer_env(dev = [7.832101630613496,0.4323776925279945],env = DevLunarLander(winds, turbulences, (0.0, 0.0)))
+    # #instantiate transfer env
+    # fin = transfer_env(dev = [7.832101630613496,0.4323776925279945],env = DevLunarLander(winds, turbulences, (0.0, 0.0)))
 
-    # load dev from the csv
-    dev = np.array([7.832101630613496,0.4323776925279945])
-
-
-
+    key ='eval' #use to switch between eval and trace
+    load_dir = '/usr0/home/parvk/cj_project/STL-Robustness/models/car_run_ppo_vanilla/model_save/model.pt'
+    speed = [5.0, 35.0]
+    steering = [0.2, 0.8]
+    env = DevCarRun(load_dir, speed, steering)
+    phi = SafetyProp()
+    fin = transfer_env(dev = [0.2,5.0],env = DevCarRun(load_dir, speed, steering),episode_len=200, agent=PPOVanilla(load_dir))
 
 
     #instantiate the model correctly based on the environment
-    dev_dim = dev.shape[0] 
+    dev_dim = 2
     action_d = fin.action_space.shape[0] #env.action_space
     emb_dim = dev_dim #random and not used anywhere since i am not learning any embeddings anyways
     hid_dim = 256
@@ -144,8 +220,12 @@ if __name__ == '__main__':
     lr=0.001
     weight_decay=0.001
     opt = optimizer(filter.parameters(), lr=lr, weight_decay=weight_decay)
-    dev_file_path =''
+    dev_file_path = 'violations/car_run.csv'
     # for loading
     # filter.load_state_dict(torch.load('filter_weights.pth'))
-    train(filter, fin, dev_file_path, opt)
+    if key == 'train':
+        train(filter, fin, dev_file_path, opt, 'car_run')
+    else:
+        filter.load_state_dict(torch.load('model_weights/car_run/filter_checkpoint_20240517_132838.pth'))
+        eval(filter, fin, False, dev_file_path)
 
--- scripts/training/transfer_lunar_lander.py
+++ scripts/training/transfer_lunar_lander.py
@@ -1,35 +1,36 @@
 from robustness.envs.lunar_lander import DevLunarLander, SafetyProp, SafetyProp2
 import time
 import imageio
-winds = [0.0, 10.0]  
+
+winds = [0.0, 10.0]
 turbulences = [0.0, 1.0]
-env = DevLunarLander(winds, turbulences, (0.0, 0.0))
+env = DevLunarLander(winds, turbulences, (5.0, 0.5))
 from stable_baselines3 import PPO as BaselinePPO
 
 
 episode_len = 300
-#env, x0bounds = env.instantiate([7.832101630613496,0.4323776925279945])
-env, x0bounds = env.instantiate([6.8851463628585545,0.6669314213708963])
+env, x0bounds = env.instantiate([7.843638754739874,0.505699763079408])
+#env, x0bounds = env.instantiate([6.8851463628585545,0.6669314213708963])
 #env, x0bounds = env.instantiate([5.879106789841222, 0.5553259436583973])
-agent = BaselinePPO.load('/usr0/home/parvk/cj_project/STL-Robustness/models/lunar-lander/ppo.zip', env)  
+agent = BaselinePPO.load('/usr0/home/parvk/cj_project/STL-Robustness/models/lunar-lander/ppo2.zip', env)  
 
-# set the initial state after
-obs = env.reset_to([-1.842157832288649,-1.4106696024537086])  
-
+# # set the initial state after
+obs = env.reset_to([-0.00034799575223587453,1.40477454662323])  
+#obs = env.reset()
 
 
 # Train the agent
-agent.learn(total_timesteps=50000) 
+#agent.learn(total_timesteps=50000) 
 frames = []
 # # Save the trained model
 # model.save("ppo_lunarlander")
-obs = env.reset_to([-1.842157832288649,-1.4106696024537086])  
+# obs = env.reset_to([-1.842157832288649,-1.4106696024537086])  
 # # Load the trained model (optional)
 for _ in range(episode_len):
     action = agent.predict(obs, deterministic=True)[0]
     obs, reward, _, _ = env.step(action)
-    time.sleep(0.01) 
-    #env.render()
+    #time.sleep(0.01) 
+    env.render()
     frames.append(env.render(mode="rgb_array"))
 
-imageio.mimsave('lunar_lander_sat2.gif', frames, fps=30)
+imageio.mimsave('lunar_lander_sat2new.gif', frames, fps=30)
